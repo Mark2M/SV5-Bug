71a765ef59840911ad7f4ed3b4a0712ad0d36b3f
Mark2M <36007552+Mark2M@users.noreply.github.com>
2024-07-05 09:46:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Svelte 5 Bug
+
+## Symptoms
+- The stylesheet being imported into +layout.svelte doesn'y get import straight away when the project is built
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+
+  let {children} = $props()
+
+</script>
+
+  <div style='background-color: #f0f0f0; padding-inline: .42em; max-width: 800px; margin-inline: auto; height: 100vh;'>
+    
+    <div style="padding: .42em 1em;">
+      <a data-sveltekit-reload href="/">Home Page</a> 
+      <a data-sveltekit-reload href="/app">Another Page</a>
+    </div>
+    
+    {@render children()} 
+  </div>
+
+
+<style>
+
+  a{
+    padding-inline: 1em;
+  }
+
+</style>
